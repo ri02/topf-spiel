bc02a3c668c633ac06ac4cf60c0d49a86ed5cc08
Marie Hess <hess.marietheres@gmail.com>
2020-04-07 14:28:15 +0200

    solved bug of missing word at words#new and refactoring of words controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,49 +1,56 @@
 class WordsController < ApplicationController
+  before_action :set_game
+  before_action :set_words , only: [:new, :create]
 
   def index
     @words = Word.where(game_id: params[:game_id]).where(status: true)
-    @game = Game.find(params[:game_id])
     @word = @words.sample
   end
 
   def new
+    redirect_to game_path(@game) if (@game.number * @game.players) <= @words.count
     @word = Word.new
-    @game = Game.find(params[:game_id])
-    @words = Word.where(game_id: params[:game_id])
-    @total = @game.number * @game.players
-    @i = (1..@game.number).to_a
-    redirect_to game_path(@game) if @total == @words.count
   end
 
   def create
-    @game = Game.find(params[:game_id])
-    @words = Word.where(game_id: params[:game_id])
-    word_params[:text].each do |word|
-      @word = Word.new(text: word)
-      @word.game_id = params[:game_id]
-      @total = @game.number * @game.players
-
-
-        if @word.save
-
-        else
-        render :new
-        end
+    if word_params[:text].length > word_validation
+     render :new
+    elsif (@game.number * @game.players) <= @words.count
+      redirect_to game_path(@game)
+    else
+      word_params[:text].each do |word|
+        @word = Word.new(text: word, game_id: params[:game_id])
+        @word.save
+      end
+      redirect_to game_path(@game)
     end
-    redirect_to game_path(@game)
   end
-
-
 
   def update
     @word = Word.find(word_params[:id])
-    @game = Game.find(params[:game_id])
     @word.update(status: false)
     redirect_to game_words_path(@game)
   end
 
 
   private
+
+  def word_validation
+    i = 0
+    word_params[:text].each do |word|
+      @word = Word.new(text: word, game_id: params[:game_id])
+      i += 1 if @word.valid?
+    end
+    i
+  end
+
+  def set_game
+    @game = Game.find(params[:game_id])
+  end
+
+  def set_words
+    @words = Word.where(game_id: params[:game_id])
+  end
 
   def word_params
     params.require(:word).permit(:id, text:[])
