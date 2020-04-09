4357c17ec4b9acde2949806a6ca7dae93984e13e
Marie Hess <hess.marietheres@gmail.com>
2020-04-09 17:21:02 +0200

    groups aus datenbank enfernt

---
+++ db/migrate/20200409151507_drop_groups_table.rb
@@ -0,0 +1,9 @@
+class DropGroupsTable < ActiveRecord::Migration[5.2]
+ def up
+    drop_table :groups
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_123838) do
+ActiveRecord::Schema.define(version: 2020_04_09_151507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,14 +48,6 @@ ActiveRecord::Schema.define(version: 2020_04_09_123838) do
     t.integer "counter_b", default: 0
   end
 
-  create_table "groups", force: :cascade do |t|
-    t.integer "counter", default: 0
-    t.bigint "game_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["game_id"], name: "index_groups_on_game_id"
-  end
-
   create_table "words", force: :cascade do |t|
     t.string "text"
     t.bigint "game_id"
@@ -66,6 +58,5 @@ ActiveRecord::Schema.define(version: 2020_04_09_123838) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "groups", "games"
   add_foreign_key "words", "games"
 end
